6ff85299f909650edf47af09557312cc30ef0bab
jakeski2020 <26joliviero@derryfield.org>
2026-04-03 13:33:59 -0400

    homework 27

---
+++ HW 27/org/derryfield/math/Geometry.java
@@ -0,0 +1,11 @@
+package org.derryfield.math;
+public class Geometry {
+        public static double get_perimeter_square(double s) {
+        return 4 * s;
+    }
+    public static double get_area_square(double s) {
+        return s * s;
+    }
+}
+
+
